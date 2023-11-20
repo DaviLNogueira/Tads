723715d7f86cfd2f2fa288a441ba13e12d2b5aae
Nogueira <davi_nogueira@ufms.br>
2023-11-20 08:48:25 -0400

    feat : migration e sawagger

---
+++ database/migrations/2023_11_06_161300_create_assinaturas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assinaturas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('data_inicio')->nullable(false);
+            $table->timestamp('data_termino')->nullable(false);
+            $table->enum('status',['ativo','cancelado','expirado']);
+            $table->foreignId('planos');
+            $table->integer('cliente_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assinaturas');
+    }
+};
