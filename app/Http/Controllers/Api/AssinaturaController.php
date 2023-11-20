85ef132df09e2a7d100afbdcb2c6b6bdf653a0ae
Nogueira <davi_nogueira@ufms.br>
2023-11-20 08:30:17 -0400

    feat swagger automatico

---
+++ app/Http/Controllers/Api/AssinaturaController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Assinatura;
+use App\Models\Plano;
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class AssinaturaController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/assinaturas",
+     *     summary="Display a listing of signatures",
+     *     description="Retrieves a list of all signatures",
+     *     tags={"Signatures"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation"
+     *     )
+     * )
+     */
+    public function index()
+    {
+        return Assinatura::all();
+    }
+
+    public function store(Request $request)
+    {
+        $assinaturaRequest = $request->all();
+        $planoId = $assinaturaRequest['plano'];
+        $cliente = $assinaturaRequest['cliente'];
+        $vigencia = $assinaturaRequest['vigencia'];
+        $plano = Plano::find($planoId);
+
+        try {
+//            $plano = new Assinatura::create([
+//                'data_inicio' =>new DateTime('now'),
+//                'data_fim' => (new DateTime())->add($vigencia),
+//                'cliente_id' => $cliente,
+//                'status' => 'ativo',
+//                'plano' => $plano,
+//
+//            ]);
+            $plano->save();
+
+        }catch (\Exception $e){
+            Log::alert($e->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Assinatura::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $assinaturaRequest = $request->all();
+        $assinatura = Assinatura::find($id);
+        $assinatura->data_termino = $assinaturaRequest['data_termino'];
+        $assinatura->status = $assinaturaRequest['status'];
+        $assinatura->save();
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $assinatura = Assinatura::find($id);
+        $assinatura->delete();
+    }
+}
---
+++ app/Models/Assinatura.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assinatura extends Model
+{
+    use HasFactory;
+
+    protected $table = 'assinaturas';
+
+    protected $fillable = [
+        'data_inicio',
+        'data_termino',
+        'status',
+        'plano_id',
+        'cliente_id'
+    ];
+}
